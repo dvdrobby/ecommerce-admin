ab67374c29adb823c4f1465eb960fd8087b97267
dvdrobby <david.robby.sanjaya@gmail.com>
2023-07-24 16:11:49 +0700

    install zustand and add Modals for creating Store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  
+
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/"></UserButton>    
+      Root Page
     </div>
     
   )
